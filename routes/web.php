0be7ab267a175c4fc48d963c0e718db2a7d8cbe7
alamsyahalamm <muhammad728744@smk.belajar.id>
2023-10-13 13:56:02 +0700

    ubdate

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CastController;
+
+
+
+
 
 
 /*
@@ -29,5 +34,26 @@ Route::get('/data-table', function () {
     return view('halaman.data-Table');
 });
 
+// crud
+Route::get('/cast/create', [CastController::class, 'create']);
 
+// untuk kirim data kedatabase
+Route::post('/cast', [CastController::class, 'store']);
+
+
+// tampil semua datanya
+
+Route::get('/cast', [CastController::class, 'index']);
+
+// detail cast
+Route::get('/cast/{cast_id}',[CastController::class, 'show']);
+
+// ubdate
+Route::get('/cast/{cast_id}/edit', [CastController::class, 'edit']);
+
+// ubdate kedatabase sesuai id
+Route::put('/cast/{cast_id}',[CastController::class, 'update']);
+
+// HAPUS BERDASARKAN ID
+Route::delete('/cast/{cast_id}',[CastController::class, 'destroy']);
 
